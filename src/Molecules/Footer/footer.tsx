5891dc4684e467e443d90d66262ae4d4285abfd6
WS7EVEN <emmanoelw@gmail.com>
2023-06-21 00:22:21 -0300

    feat: Main structure created

--- src/Molecules/Footer/footer.tsx
+++ src/Molecules/Footer/footer.tsx
@@ -1,12 +1,22 @@
-export default function Footer () {
-    return (
-        <footer>
-            <div><p>Kenai</p></div>
-            <div>
-                <span>i</span>
-                <span>i</span>
-                <span>i</span>
-            </div>
-        </footer>
-    )
+import { CiInstagram, CiFacebook, CiTwitter } from "react-icons/ci";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__logo">
+        <p>Kenai</p>
+      </div>
+      <div className="footer__socialMedia">
+        <span>
+          <CiInstagram></CiInstagram>
+        </span>
+        <span>
+          <CiFacebook></CiFacebook>
+        </span>
+        <span>
+          <CiTwitter></CiTwitter>
+        </span>
+      </div>
+    </footer>
+  );
 }
--- src/Organisms/MainTitle/index.tsx
+++ src/Organisms/MainTitle/index.tsx
@@ -4,43 +4,157 @@ import { AiFillStar } from "react-icons/ai";
 export default function Navbar() {
   return (
     <section className="mainTitle">
-      <span className="mainTitle__category">Ação</span>
-
-      <span>
-        <AiFillStar></AiFillStar>
-      </span>
-      <span>Icon</span>
-      <span>Icon</span>
-      <span>Icon</span>
-      <span>Icon</span>
-
-      <h1>Batman</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto
-        voluptatibus quia reiciendis nobis unde. Obcaecati dolor fuga qui vitae
-        atque reiciendis asperiores voluptates repellendus totam dolorem, in, ad
-        ullam. Dolores.
-      </p>
-
-      <a>Assistir agora</a>
       <div>
-        <h2> Populares </h2>
+        <span className="mainTitle__category">Ação</span>
         <div>
-          {" "}
+          <div>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+          </div>
+
+          <div>
+            <span>time</span>
+            <span>1h 57m</span>
+          </div>
+        </div>
+
+        <div>
+          <h1 className="mainTitle__movieName">Batman</h1>
+          <p className="mainTitle__movieDescription">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto
+            voluptatibus quia reiciendis nobis unde. Obcaecati dolor fuga qui
+            vitae atque reiciendis asperiores voluptates repellendus totam
+            dolorem, in, ad ullam. Dolores.
+          </p>
+
+          <a className="mainTitle__mainButton">Assistir agora</a>
+        </div>
+      </div>
+
+      <div className="popularMovies">
+        <h2> Populares </h2>
+        <div className="popularMovies__movie">
+          <span className="mainTitle__category">Categoria</span>
+          <div>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+          </div>
+
           <p>Shang Chi</p>
         </div>
-        <div>
-          {" "}
-          <p>Wisa Dragon</p>
+        <div className="popularMovies__movie">
+          <span className="mainTitle__category">Categoria</span>
+          <div>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+
+            <p>Wisa Dragon</p>
+          </div>
         </div>
-        <div>
-          <p>Loki</p>
+        <div className="popularMovies__movie">
+          <span className="mainTitle__category">Categoria</span>
+          <div>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+
+            <p>Loki</p>
+          </div>
         </div>
-        <div>
-          <p>Spider Man</p>
+        <div className="popularMovies__movie">
+          <span className="mainTitle__category">Categoria</span>
+          <div>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+
+            <p>Spider Man</p>
+          </div>
         </div>
-        <div>
-          <p>The Witcher</p>
+        <div className="popularMovies__movie">
+          <span className="mainTitle__category">Categoria</span>
+          <div>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+            <span>
+              <AiFillStar></AiFillStar>
+            </span>
+
+            <p>The Witcher</p>
+          </div>
         </div>
       </div>
     </section>
